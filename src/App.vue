361f1641a55eb2142c77072aa03d5616ede1cac8
SajjadHadi <sajjadhadi77@gmail.com>
2025-03-17 10:24:16 +0100

    Add drawer w/o navigation

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <script setup lang="ts">
-
 import PlaylistForm from "./components/library/PlaylistForm.vue";
 import PlaylistsList from "./components/library/PlaylistsList.vue";
 import AudioPlayer from "./components/player/AudioPlayer.vue";
 import TracksList from "./components/library/TracksList.vue";
+import Drawer from "./components/common/Drawer.vue";
+
 </script>
 
 <template>
   <v-app>
-    <AudioPlayer/>
-    <h2>Tracks List</h2>
-    <TracksList/>
+    <Drawer/>
 
-    <h2>Playlists</h2>
-    <PlaylistsList/>
+    <!-- Main content area -->
+    <v-main class="ml-14">
+      <v-container fluid>
+        <AudioPlayer/>
 
-    <h2>Create a Playlist</h2>
-    <PlaylistForm/>
+        <!-- Tracks List Section -->
+        <v-row>
+          <v-col>
+            <h2>Tracks List</h2>
+            <TracksList/>
+          </v-col>
+        </v-row>
+
+        <!-- Playlists Section -->
+        <v-row>
+          <v-col>
+            <h2>Playlists</h2>
+            <PlaylistsList/>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <h2>Create a Playlist</h2>
+            <PlaylistForm/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
-<style>
-html, body {
+<style scoped>
+html,
+body,
+#app,
+.v-application {
   height: 100%;
   margin: 0;
   padding: 0;
---
+++ src/components/common/Drawer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const drawer = ref(true);
+const rail = ref(true);
+</script>
+
+
+<template>
+  <v-navigation-drawer
+      v-model="drawer"
+      :rail="rail"
+      permanent
+      @click="rail = false"
+  >
+    <v-list-item
+        prepend-icon="mdi-music-clef-treble"
+        title="Harmoniq Player"
+        nav
+    >
+      <template v-slot:append>
+        <v-btn
+            icon="mdi-chevron-left"
+            variant="text"
+            @click.stop="rail = !rail"
+        />
+      </template>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home" title="Home" value="home"/>
+      <v-list-item prepend-icon="mdi-playlist-music" title="Playlists" value="playlists"/>
+    </v-list>
+  </v-navigation-drawer>
+</template>
